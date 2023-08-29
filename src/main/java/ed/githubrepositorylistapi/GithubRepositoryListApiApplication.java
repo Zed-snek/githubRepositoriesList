9692f4c240ca1d37a2010a21f5880ea21fb766e6
Zed-snek <ed.savchuker@gmail.com>
2023-08-29 17:26:25 +0200

    Project created

---
+++ src/main/java/ed/githubrepositorylistapi/GithubRepositoryListApiApplication.java
@@ -0,0 +1,13 @@
+package ed.githubrepositorylistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoryListApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ed/githubrepositorylistapi/GithubRepositoryListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ed.githubrepositorylistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
